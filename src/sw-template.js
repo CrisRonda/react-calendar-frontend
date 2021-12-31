173dd9b4038ef67da4683fc747d35e7f67528259
CrisRonda <cristian.j005@gmail.com>
2021-12-30 19:40:38 -0500

    updated manifest

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,11 +1,13 @@
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js'
 );
+
+workbox.loadModule('workbox-background-sync');
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
-
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 registerRoute(
   new RegExp(
     'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
@@ -28,4 +30,32 @@ registerRoute(
 registerRoute(
   new RegExp('http://localhost:4000/api/events'),
   new NetworkFirst()
+);
+
+// offline POST
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'POST'
+);
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'DELETE'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'PUT'
 );
